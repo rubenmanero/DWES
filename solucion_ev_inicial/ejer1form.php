56fa68d17e67805fb4410d38940267da72e78ffb
rubenmanero <ruben.manero@cpilosenlaces.com>
2023-09-21 12:40:38 +0200

    Se quitan lineas de comentario Se formatea el codigo

--- solucion_ev_inicial/ejer1form.php
+++ solucion_ev_inicial/ejer1form.php
@@ -6,29 +6,24 @@
 	</head>
 	<body>
 		<?php
-		//prueba de commit
-		//comentario
-if (isset($_REQUEST["entero"]))
-{
-    $entero=$_REQUEST["entero"];
-    if ($entero==0) {
-        echo $entero, " es neutro.";
-    } else {
-        if ($entero%2==0) {
-            echo $entero, " es par.";
-        } else {
-            echo $entero, " es impar.";
-        }
-    }
-}
-else
-{
-	echo '
-	<form action="ejer1form.php" method="GET">
-	Introduce un número entero: <input type="number" name="entero" step="1" required>
-	<input type="submit" value="Enviar">
-	</form>';
-}
+            if (isset($_REQUEST["entero"])) {
+                $entero = $_REQUEST["entero"];
+                if ($entero == 0) {
+                    echo $entero, " es neutro.";
+                } else {
+                    if ($entero % 2 == 0) {
+						echo $entero, " es par.";
+                    } else {
+                        echo $entero, " es impar.";
+                    }
+                }
+            } else {
+                print '
+				<form action="ejer1form.php" method="GET">
+				Introduce un número entero: <input type="number" name="entero" step="1" required>
+				<input type="submit" value="Enviar">
+				</form>';
+            }
 		?>
 		<a href="ejer1form.html"><br/><br/>Introducir otro número</a>
 	</body>
